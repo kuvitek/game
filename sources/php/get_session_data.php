08816909ee19f4c0afc8768b949423658c15bb70
kuvitek <kukvitek@gmail.com>
2019-04-07 19:36:43 +0300

    Delete get_session_data.php

--- sources/php/get_session_data.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    if (!isset($_SESSION['userName'])) {
-        $_SESSION['userName'] = "";
-        $_SESSION['accessToken'] = "";    
-    };
-    echo json_encode($_SESSION);
